db16b9067c8c52634a8e14ddebdb53e2f60a41f3
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2021-08-02 15:46:21 -0300

    feito

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>//Biblioteca que indentifica a  programação C++
+
+int main(){
+    
+    using namespace std;// para não uusa   std e o ::
+
+    int n1=0, n2 =0; // sera varivel para  não  ter lixo
+    
+    std :: cout<<"Ola Mundo!!!";// comando de esibição
+    //---------------------------------------------
+        return 0;
+    
