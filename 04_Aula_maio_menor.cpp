5bc94d44d4f6dfc88121ddeaeb40afefe27b2e99
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2021-08-03 20:56:56 -0300

    feito

---
+++ 04_Aula_maio_menor.cpp
@@ -0,0 +1,20 @@
+// Apresatando if e  o Else
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero1 =0,  numero2 =0;
+
+        cout<< "Digitr  primero  numero";
+        cin >> numero1;
+
+        cout<< "Digitr  cegundo numero";
+        cin >> numero2;
+        //1;51;08
+
+        if
+
+    return 0;
+}
