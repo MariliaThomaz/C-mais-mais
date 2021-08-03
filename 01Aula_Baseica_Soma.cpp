f140e1c79ffd64e4672911d735e79685fb6443b4
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2021-08-02 22:04:09 -0300

    feito

---
+++ 01Aula_Baseica_Soma.cpp
@@ -0,0 +1,38 @@
+#include<iostream> // aqui  esmos falnado  que imamnado para com putador  que  trabanhandop com C++
+// Para  ineficar  que é C++ a Biblioteca que é indentifica a  progamação c+++
+// modo para enterder é biblioteteca  mas  modo  correto é  chamdo de Reder (Rider)
+// Quer dizer #include<iostream> é Cabesario
+// Cabesario quer diz  que estopu  incluindo todo Paradiquima do C++
+
+int main() // digitar Só main ele  abre  tudo
+//nos  obisear  Main  ela  do timpo Int ela  trabalga  com doisa  tiposa de Retono
+//Se tudo  dar certo ela tem um retono 0
+//Se der aguma  coisa difernte ela dara retono difernte  de 0
+
+{
+    //no C++ Exite doisa  fluxo o de entra de  de saida
+
+    std :: cout<<"Ola Mundo2!!"; // pata escrever na tela  ou exibir  é  COUT<<
+// PARA interder as --- << -- lembra  domo flxo 
+//
+
+int nume1 = 0, nume2 = 0;//Toda ves que voce  cria varivel  iquando a  0 poisa toda ves  que
+// voce  pede  para software criar um varivel ele  armazena  um espaçoa na memoria Ram
+// E voce incilisando ela evita que ela amazene lixo não desjado
+
+
+    std :: cout<<"Digite um numero 1=>";// aqui  mosata o que  mesagem 
+
+    std ::  cin >> nume1;// para  ler  o que usario digitol  no caso
+    // cin  pega  o que usario  digitol e  manda  para varivel  
+
+    std :: cout<<"Digite um numero 2=>";// aqui  mosata o que  mesagem 
+    
+    std ::  cin >> nume2;// para  ler  o que usario digitol  no caso
+    // cin  pega  o que usario  digitol e  manda  para varivel  
+
+    //Soma direta com Saida de dados com - cout
+    std :: cout <<"A soma "<<(nume1+ nume2);//aqui estou  fazendo uma soma direta sem cria  uma varivel alsilar 
+    // para exibir  resulatado  da soma << (nume1 + nume2)
+    return 0;// quando eu retono   
+}
