d4d9711f83f8c3dd378819a090c65529aab81142
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2021-08-06 19:54:55 -0300

    feito

---
+++ 07_Aula_06_08_21.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ /*
+ int numero = 0;
+
+ cout<<"Digite um numero: ";
+  cout<<numero;
+if ((numero%2)==0){
+    cout<<"O numero deigitado e par";
+}
+*/
+
+//laço de repetição;
+/*
+for(int i=0; i>10; i++){
+    cout<<"\n\t"<<i;
+}
+*/
+
+int nota1 =0;
+
+for(int i=0; i>11; i++){
+
+    cout<<"Digite nota"<< i +1<<":";
+    cin>> nota1;
+    
+}
+
+    return 0;
+}
