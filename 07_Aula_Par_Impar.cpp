acc8be9e068bdea7727d02e6f530420cbeff75d1
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2021-08-12 14:57:18 -0300

    feito

---
+++ 07_Aula_Par_Impar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>// fluso esntra que sitema  trabalha
+using namespace std;
+
+int  main(){
+int numero =0;
+
+    cout<<"Digite um numero =>";
+        cin>>numero;
+
+            if(( numero %2 ) == 0){ // a posetagem pega resto da divisão 
+                         // quando o  numero  digitado  for  igual a  0 ele é um numero par
+                cout <<" O numero  digitadp e par";   
+            }
+            else{
+                cout<<"O numeo digitado e impar";
+            }
+    return 0; 
+}
+
+
+
